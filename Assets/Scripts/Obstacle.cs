2c87c53cb4d36085273d4cf50397a481caaaebb1
gustavlundgren <sgvln005@edu.karlstad.se>
2023-07-28 14:40:45 +0200

    Health system

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    void Update()
+    public HealthBar healthBar;
+    private HealthSystem healthSystem;
+
+    private void Start()
     {
-        
+        healthSystem = new HealthSystem(100);
+
+        healthBar.SetUp(healthSystem);
+    }
+
+    private void Update()
+    {  
+        Debug.Log(healthSystem.GetHealth());
+        healthSystem.Damage(1);
     }
 }
